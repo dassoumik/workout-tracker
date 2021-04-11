b1cc7a2cad095e8bf585bb3b1761d5945578899c
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-04-11 08:36:25 -0400

    add the initial get route

--- routes/api/workouts.js
+++ routes/api/workouts.js
@@ -1,5 +1,15 @@
-const workouts = () => {
-    console.log("in workouts");
-}
+const router = require("express").Router();
+const Exercise = require("../../models/exercise.js");
 
-module.exports = workouts;
+router.get("/api/workouts", (req, res) => {
+    Exercise.find({})
+      .sort({ date: -1 })
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
+module.exports = router;
