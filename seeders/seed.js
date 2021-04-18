5d130188a5e45aed37fe4ea47a40a910b18091f9
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-04-18 09:21:10 -0400

    remove toplogy and index

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 // let db = require("../models");
 const mongojs = require("mongojs");
 
-mongoose.connect("mongodb://localhost/workouts", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
